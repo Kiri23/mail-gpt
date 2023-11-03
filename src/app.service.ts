b20c0ffa9933c88a8fab2c235d5e8235248eb75b
kiri23 <christian@opticpower.com>
2023-11-02 22:53:11 -0400

    UI: add card emails

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,35 @@
 import { Injectable } from '@nestjs/common';
 
 interface User {
-  name: string;
+  author: string;
+  category?: string;
+  description?: string;
+  link?: string;
 }
 
 export interface AppServiceResponse {
-  users: User[];
+  emails: User[];
 }
 
 @Injectable()
 export class AppService {
   getHello(): AppServiceResponse {
     return {
-      users: [{ name: 'Max' }, { name: 'Bob' }],
+      emails: [
+        {
+          author: 'Max',
+          category: 'newsletter',
+          description: 'give your one second deal. Dont wait unitl is to late',
+          link: '#',
+        },
+        {
+          author: 'Bob',
+          category: 'Ai',
+          description:
+            'AI is breaking the world. Check out this tool from future tool',
+          link: '#',
+        },
+      ],
     };
   }
 }
--- src/views/users/index.jsx
+++ src/views/users/index.jsx
@@ -1,5 +1,13 @@
-<ul class="users">
-  {users.map(user => (
-    <li key={user}>{user.name}</li>
-  ))}
-</ul>
+<>
+  {emails.map(email => (
+    <div class="card" key={email}>
+      <div class="card-body">
+        <h5 class="card-title">{email.author}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">{email.category}</h6>
+        <p class="card-text">{email.description}</p>
+        <a href={email.link} class="card-link">Read more</a>
+      </div>
+  </div>
+    ))}
+</>
+
