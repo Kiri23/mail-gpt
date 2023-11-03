a2d213a91ba73b7825eeb65f45445e704a94efc9
kiri23 <christian@opticpower.com>
2023-11-03 08:51:23 -0400

    Add comment explaining why I add a inteface

--- src/emails/emailService.interface.ts
+++ src/emails/emailService.interface.ts
@@ -1,6 +1,8 @@
+// creating an interface so in the future we want to add Gmail support
 export const EMAIL_SERVICE = 'EMAIL SERVICE';
 
 export interface IEmailService {
   getEmails();
+  // TODO: add typescript type as a example below
   //   getEmails(): Promise<string>;
 }
