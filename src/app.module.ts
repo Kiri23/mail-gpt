5c249d71f4e90f4bcf0d4f13a3e70b89fbe3abea
kiri23 <christian@opticpower.com>
2023-11-06 21:12:50 -0400

    Add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmailModule } from './emails/emails.module';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [EmailModule],
+  imports: [ConfigModule.forRoot(), EmailModule],
   controllers: [AppController],
   providers: [],
 })
