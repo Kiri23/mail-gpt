70250d39dab747d368a0274f58f472af0415cddd
kiri23 <christian@opticpower.com>
2023-11-06 21:13:09 -0400

    Add user profile icon on navbar

--- src/views/navbar.jsx
+++ src/views/navbar.jsx
@@ -14,6 +14,17 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <div class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle ms-4 text-body" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <i class="bi bi-person-circle fs-5"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#">My Account</a></li>
+          <li><a class="dropdown-item" href="#">Settings</a></li>
+          <li> <hr class="dropdown-divider"/></li>
+          <li><a class="dropdown-item" href="#">Logout</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </nav>
