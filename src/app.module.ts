a5603d72bd0cbc1e744ff8c27123caa8fbbcc657
kiri23 <christian@opticpower.com>
2023-11-09 23:13:36 -0400

    add logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { EmailModule } from './emails/emails.module';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import validationSchema from './config/configuration.schema';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
-  imports: [ConfigModule.forRoot(), EmailModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+    EmailModule,
+    UserModule,
+  ],
   controllers: [AppController, UserController],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
