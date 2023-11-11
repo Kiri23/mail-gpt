bcc4ebbc0d777d5c41980bd81050c3f3da6f8e3c
kiri23 <christian@opticpower.com>
2023-11-11 15:18:19 -0400

    Replace JSX with handlebars & include alpine.js to solve cors issue. HTMX do ajax request to third party and they are blocked by microsoft
    it has more support for different backend
    it's not so restrictive as I can have html attributes with : and @

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { CustomLogger } from './customLogger';
-const engine = require('express-engine-jsx');
+import { CustomLogger } from './logger/customLogger';
+import { engine } from 'express-handlebars';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
@@ -11,8 +11,8 @@ async function bootstrap() {
   });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'src', 'views'));
-  app.setViewEngine('jsx');
-  app.engine('jsx', engine);
+  app.setViewEngine('hbs');
+  app.engine('hbs', engine({ extname: '.hbs' }));
 
   // app.enableCors();
   await app.listen(3001);
